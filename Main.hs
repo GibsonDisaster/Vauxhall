95dfbca6e49928d090afbd2c63cf76473dee7900
Henning Tonko <henninghtonko@gmail.com>
2017-12-23 18:16:36 -0600

    added items, picking them up, and enemies

--- Main.hs
+++ Main.hs
@@ -2,17 +2,24 @@ module Main where
   import Prelude hiding (Either(..))
   import System.Console.ANSI
   import System.IO
+  import System.Random
   import qualified Data.Map.Strict as M
   import Types
 
   wall1 :: [String]
   wall1 = ["-----------------------     ----------",
-           "|                     |     |        |",
-           "|                     ---|---        |",
-           "|                        +           |",
+           "|            s        |     |        |",
+           "|               O     ---|---        |",
+           "|          p             +           |",
            "|                     ---|---        |",
            "|                     |     |        |",
            "-----------------------     ----------"]
+
+  {-
+  Constants that must be used when determining a position
+  because of ANSI's insane coordinate system.
+  (I think this might actually be my fault)
+  -}
 
   rConst :: Coord
   rConst = (1, -1)
@@ -31,6 +38,18 @@ module Main where
 
   flipCoord :: Coord -> Coord
   flipCoord (x, y) = (y, x)
+
+  getItem :: Char -> Item
+  getItem 's' = Sword
+  getItem 'O' = Shield
+  getItem 'p' = Potion
+  getItem _ = Null
+
+  getVect :: Direction -> Coord
+  getVect Up = (-1, 0) |+| uConst
+  getVect Down = (1, 0) |+| dConst
+  getVect Left = (0, -1) |+| lConst
+  getVect Right = (0, 1) |+| rConst
 
   isImpassible :: M.Map Coord Char -> Coord -> Bool
   isImpassible m coord = case M.lookup coord m of
@@ -57,6 +76,9 @@ module Main where
   (|+|) :: Coord -> Coord -> Coord
   (|+|) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
+  (|-|) :: Coord -> Coord -> Coord
+  (|-|) (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
+
   isThatChar :: Coord -> Char -> M.Map Coord Char -> Bool
   isThatChar c ch m = case M.lookup c m of
                        Nothing -> False
@@ -69,6 +91,20 @@ module Main where
     where
       d = M.delete coord m
       r = M.insert coord c2 d
+
+  randChoice :: [a] -> IO a
+  randChoice l = do
+    n <- (randomRIO (0, ((length l) - 1)))
+    return (l !! n)
+
+  moveEnemy :: World -> Enemy -> IO Enemy
+  moveEnemy w e = do
+    dir <- randChoice [Up, Down, Left, Right]
+    let newCoord = (getVect dir) |+| (eCoord e)
+    return e { eCoord = if (isImpassible (tileMap w) newCoord) then newCoord else eCoord e, eOldCoord = flipCoord $ eCoord e }
+
+  moveEnemies :: World -> [Enemy] -> IO [Enemy]
+  moveEnemies w es = mapM (moveEnemy w) es
 
   getInput :: IO Event
   getInput = do
@@ -85,6 +121,8 @@ module Main where
       'l' -> return (Dir Right)
       'o' -> return (PlayerAction OpenDoor)
       'c' -> return (PlayerAction CloseDoor)
+      ',' -> return (PlayerAction PickUp)
+      'i' -> return (PlayerAction ShowInv)
       _   -> getInput
 
   handleExit :: IO ()
@@ -97,7 +135,8 @@ module Main where
 
   handleEvent :: World -> Event -> IO ()
   handleEvent w (Dir d) = do
-    gameLoop w { wHero = if (isImpassible (tileMap w) (flipCoord newHCoord)) then (wHero w) else newH }
+    ems <- moveEnemies w (wEnemies w)
+    gameLoop w { wHero = if (isImpassible (tileMap w) (flipCoord newHCoord)) then (wHero w) else newH, wEnemies = ems }
     where
       oldH = hCoord (wHero w)
       newHCoord = case d of
@@ -105,7 +144,7 @@ module Main where
                    Right -> (0, 1) |+| oldH
                    Up -> (-1, 0) |+| oldH
                    Down -> (1, 0) |+| oldH
-      newH = Hero {hCoord = newHCoord, hOldCoord = oldH}
+      newH = Hero { hCoord = newHCoord, hOldCoord = oldH, hHealth = hHealth (wHero w), items = items (wHero w)}
   handleEvent w (PlayerAction OpenDoor) = do
     dir <- getInput
     let t = case dir of
@@ -124,7 +163,22 @@ module Main where
              Dir Down -> (1, 0) |+| (flipCoord (hCoord (wHero w)) |+| dConst)
              _ -> (hCoord (wHero w))
     gameLoop w { tileMap = changeTile t 'o' '+' (tileMap w) }
-
+  handleEvent w (PlayerAction PickUp) = do
+    let i = case M.lookup (flipCoord (hCoord (wHero w))) (tileMap w) of
+             Nothing -> ' '
+             Just c -> c
+    let newHero = (wHero w) { items = (items (wHero w)) ++ [getItem i] }
+    let oldHero = (wHero w)
+    gameLoop w { tileMap = changeTile (flipCoord (hCoord (wHero w))) i ' ' (tileMap w), wHero = if i == ' ' then oldHero else newHero }
+  handleEvent w (PlayerAction ShowInv) = do
+    setCursorPosition 0 0
+    putStrLn "Inventory"
+    putStrLn "========="
+    mapM_ putStrLn (map show $ items (wHero w))
+    _ <- getInput
+    setCursorPosition 0 0
+    mapM_ (\_ -> putStrLn "               ") (map show $ items (wHero w))
+    gameLoop w
 
   gameLoop :: World -> IO ()
   gameLoop w = do
@@ -139,10 +193,16 @@ module Main where
       where
         l = M.toList m
 
+  drawEnemies :: [Enemy] -> IO ()
+  drawEnemies e
+    | length e == 1 = do {setCursorPosition (fst (eCoord (head e))) (snd (eCoord (head e))); putChar 'M'; setCursorPosition (fst (eOldCoord (head e))) (snd (eOldCoord (head e))); putChar ' '}
+    | otherwise = do {setCursorPosition (fst (eCoord (head e))) (snd (eCoord (head e))); putChar 'M'; setCursorPosition (fst (eOldCoord (head e))) (snd (eOldCoord (head e))); putChar 'M'; drawEnemies (tail e)}
+
   drawWorld :: World -> IO ()
   drawWorld w = do
     setCursorPosition 0 0
     drawMap (tileMap w)
+    drawEnemies (wEnemies w)
     setCursorPosition (fst $ hOldCoord (wHero w)) (snd $ hOldCoord (wHero w))
     putChar ' '
     setCursorPosition (fst $ hCoord (wHero w)) (snd $ hCoord (wHero w))
@@ -156,6 +216,10 @@ module Main where
     hideCursor
     setTitle "Vauxhall"
     clearScreen
-    let w = World { wHero = Hero {hCoord = (1, 1), hOldCoord = (30, 0)}, walls = wall1, tileMap = wall1Mapped }
+    let w = World { wHero = Hero {hCoord = (1, 1), hOldCoord = (30, 0), hHealth = 10, items = []}, 
+                    walls = wall1,
+                    tileMap = wall1Mapped,
+                    wEnemies=[Enemy {eCoord = (3, 33), eOldCoord = (0,0), eHealth = 10}]
+                  }
     drawWorld w
     gameLoop w
--- Types.hs
+++ Types.hs
@@ -3,23 +3,36 @@ module Types where
 
   type Coord = (Int, Int)
 
+  data Item = Sword | Shield | Potion | Null
+
+  instance Show Item where
+    show Sword = "Sword"
+    show Shield = "Shield"
+    show Potion = "Potion"
+    show Null = "Null"
+
   data World = World {
                 wHero :: Hero,
                 walls :: [String],
-                tileMap :: M.Map Coord Char
+                tileMap :: M.Map Coord Char,
+                wEnemies :: [Enemy]
+               } deriving Show
+
+  data Enemy = Enemy {
+                eCoord :: Coord,
+                eOldCoord :: Coord,
+                eHealth :: Int
                } deriving Show
 
   data Hero = Hero {
                 hCoord :: Coord,
-                hOldCoord :: Coord
+                hOldCoord :: Coord,
+                hHealth :: Int,
+                items :: [Item]
               } deriving Show
-
-  data State = State {
-                currentAction :: Event
-               }
 
   data Direction = Up | Down | Left | Right deriving Show
 
-  data Action = OpenDoor | CloseDoor | Idle deriving Show
+  data Action = OpenDoor | CloseDoor | PickUp | DropItem | ShowInv | Idle deriving Show
 
   data Event = Dir Direction | Exit | PlayerAction Action deriving Show
