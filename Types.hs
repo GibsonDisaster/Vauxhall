0dbd2840ce57b4cb906a3713b88d9cc98ef11ad3
Henning Tonko <henninghtonko@gmail.com>
2018-03-06 22:06:48 -0600

    Removed Read typeclass, looks nicer now

--- Types.hs
+++ Types.hs
@@ -8,15 +8,15 @@ module Types where
 
   type Coord = (Int, Int)
 
-  data Item = Potion | Coin | Null deriving (Eq, Read, Show, Generic)
+  data Item = Potion | Coin | Null deriving (Eq, Generic)
   
-  data Direction = Up | Down | Left | Right | Stay deriving (Show, Read, Generic)
+  data Direction = Up | Down | Left | Right | Stay deriving (Show, Generic)
 
-  data Action = OpenDoor | CloseDoor | PickUp | DropItem | Rest | ShowInv | ShowStats | Idle | GoDown | GoUp | Quaff | Inspect | Buy | Kick | Save | CastSpell | Rush | Help | Debug deriving (Show, Read, Generic)
+  data Action = OpenDoor | CloseDoor | PickUp | DropItem | Rest | ShowInv | ShowStats | Idle | GoDown | GoUp | Quaff | Inspect | Buy | Kick | Save | CastSpell | Rush | Help | Debug deriving (Show, Generic)
 
-  data Event = Dir Direction | Exit | PlayerAction Action deriving (Show, Read, Generic)
+  data Event = Dir Direction | Exit | PlayerAction Action deriving (Show, Generic)
   
-  data Effect = Dmg { _eDur :: Int } | Psn { _eDur :: Int } | Slp { _eDur :: Int } | None deriving (Eq, Read, Show, Generic)
+  data Effect = Dmg { _eDur :: Int } | Psn { _eDur :: Int } | Slp { _eDur :: Int } | None deriving (Eq, Generic)
 
   data Class = Knight {
                 _kConst :: Int,
@@ -44,7 +44,7 @@ module Types where
                  _pStr :: Int,
                  _pDex :: Int,
                  _pInt :: Int
-               } deriving (Eq, Read, Show, Generic)
+               } deriving (Eq, Generic)
 
   data World = World {
                 _mode :: String,
@@ -60,13 +60,13 @@ module Types where
                 _wShops :: M.Map (Coord, String) (Item, Int),
                 _wTraps :: M.Map String [Trap],
                 _wCurrFounts :: [Fountain]
-               } deriving (Show, Read, Generic)
+               } deriving (Show, Generic)
 
   data Enemy = Enemy {
                 _eCoord :: Coord,
                 _eOldCoord :: Coord,
                 _eHealth :: Int
-               } deriving (Show, Eq, Read, Generic)
+               } deriving (Show, Eq, Generic)
 
   data Hero = Hero {
                 _hName :: String,
@@ -82,50 +82,50 @@ module Types where
                 _hMoney :: Int,
                 _hEffects :: [Effect],
                 _hSpells :: [Spell]
-              } deriving (Show, Read, Generic)
+              } deriving (Show, Generic)
 
   data Staircase = Staircase {
                     _sDir :: Char,
                     _sDest :: String,
                     _sCoord :: Coord
-                   } deriving (Show, Read, Generic)
+                   } deriving (Show, Generic)
 
   data Trap = Trap {
                 _tEffect :: Effect,
                 _tCoord :: Coord,
                 _tDuration :: Int
-              } deriving (Show, Read, Generic)
+              } deriving (Show, Generic)
 
   data Spell = Spell {
                 _spCoord :: Coord,
                 _spEffect :: Effect,
                 _spDir :: Direction
-               } deriving (Show, Read, Generic)
+               } deriving (Show, Generic)
 
                
   data Fountain = Fountain {
                     _fCoord :: Coord,
                     _fEffect :: Effect,
                     _fQuaffed :: Bool
-                  } deriving (Show, Read, Generic)
+                  } deriving (Show, Generic)
 
   {- Will need to implement custom Read class as well to use these -}
 
-  -- instance Show Class where
-  --   show (Knight _ _ _ _) = "Knight"
-  --   show (Thief _ _ _ _) = "Thief"
-  --   show (Sub _ _ _ _) = "Sub"
-  --   show (PolkaKing _ _ _ _) = "Polka King"
+  instance Show Class where
+    show (Knight _ _ _ _) = "Knight"
+    show (Thief _ _ _ _) = "Thief"
+    show (Sub _ _ _ _) = "Sub"
+    show (PolkaKing _ _ _ _) = "Polka King"
 
-  -- instance Show Item where
-  --   show Potion = "Potion"
-  --   show Coin = "Coin"
-  --   show Null = "Null"
+  instance Show Item where
+    show Potion = "Potion"
+    show Coin = "Coin"
+    show Null = "Null"
 
-  -- instance Show Effect where
-  --   show None = ""
-  --   show (Dmg d) = "Dmg " ++ (show d) ++ "t"
-  --   show (Psn d) = "Psn " ++ (show d) ++ "t"
+  instance Show Effect where
+    show None = ""
+    show (Dmg d) = "Dmg " ++ (show d) ++ "t"
+    show (Psn d) = "Psn " ++ (show d) ++ "t"
 
   instance Binary Item
   instance Binary Direction
